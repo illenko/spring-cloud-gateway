ba091671c2c1a8759c18a759d209f74536602d25
illenko <kostiaillenko@gmail.com>
2022-08-09 12:56:06 +0300

    Removed redundant logging.

--- src/main/kotlin/com/illenko/gateway/filter/AuthFilter.kt
+++ src/main/kotlin/com/illenko/gateway/filter/AuthFilter.kt
@@ -4,7 +4,6 @@ import com.illenko.gateway.config.RedisCache
 import com.illenko.gateway.config.RoutesConfig
 import com.illenko.gateway.dto.Client
 import com.illenko.gateway.util.MapperUtils
-import mu.KotlinLogging
 import org.springframework.cloud.gateway.filter.GatewayFilterChain
 import org.springframework.cloud.gateway.filter.GlobalFilter
 import org.springframework.cloud.gateway.route.Route
@@ -19,19 +18,12 @@ import reactor.core.publisher.Mono
 @Component
 class AuthFilter(private val redisCache: RedisCache) : GlobalFilter, Ordered {
 
-    private val log = KotlinLogging.logger {}
-
     override fun filter(exchange: ServerWebExchange, chain: GatewayFilterChain): Mono<Void> {
-
         val apiKeyHeader = exchange.request.headers["key"]
         val routeId = exchange.getAttribute<Route?>(ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR)?.id
-        log.debug("Handled request with API Key: $apiKeyHeader and routeId: $routeId")
-
         if (routeId == null || apiKeyHeader == null || !isAuthorized(routeId, apiKeyHeader.first())) {
-            log.debug { "Request is not authorized" }
             throw ResponseStatusException(HttpStatus.UNAUTHORIZED, "Request is not authorized")
         }
-
         return chain.filter(exchange)
     }
 
